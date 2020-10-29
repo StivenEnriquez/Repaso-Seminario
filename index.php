0158200fa557f45e44b7a96c8d40c997eb07ce8c
STIVEN ENRIQUEZ <cristianangulo1@hotmail.com>
2020-10-28 21:33:28 -0500

    Mi pirmer commit del proyecto repaso

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Repaso seminario</title>
+    
+</head>
+<body>
+    
+<h1>Hola Mundo</h1>    
+
+</body>
+</html>
